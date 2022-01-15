bf04d62498e957bd667e75fe60abd36fdbd827e4
aniket53 <akhot610@gmail.com>
2022-01-15 15:51:10 +0530

    First Try Error

--- app.js
+++ app.js
@@ -167,11 +167,14 @@ app.post("/login",function(req,res){
     });
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//   port = 3000;
+// }
 
-app.listen(port,function(req,res){
-    console.log("Server has started successfully!!");
+// app.listen(port,function(req,res){
+//     console.log("Server has started successfully!!");
+// });
+app.listen(process.env.PORT || 3000, function(){
+    console.log("OK");
 });
